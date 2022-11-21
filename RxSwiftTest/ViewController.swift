3b9bf3e095a6acbd15fb7fa99a9963a3de5f7209
teiresias22 <teiresias1987@gmail.com>
2022-11-21 23:33:39 +0900

    'Command Ld failed with a nonzero exit code' 에러 발생 원인 파악중

--- RxSwiftTest/ViewController.swift
+++ RxSwiftTest/ViewController.swift
@@ -6,14 +6,40 @@
 //
 
 import UIKit
+import RxSwift
+import RxCocoa
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var inputText: UITextField!
+    @IBOutlet weak var addButton: UIButton!
+    @IBOutlet weak var tableView: UITableView!
+    
+    var todos = [String]()
+    var todosRelay = BehaviorRelay(value: [String]())
+    let disposeBag = DisposeBag()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        self.tableView.register(UITableViewCell.self, forCellReuseIdentifier: "TodoCell")
+        
+        self.bindToRx()
     }
-
-
+    
+    func bindToRx() {
+        self.addButton.rx.tap.bind { [self] in
+            guard inputText.text?.isEmpty == false else { return }
+            
+            todos.append(self.inputText.text!)
+            todosRelay.accept(self.todos)
+            inputText.text = ""
+        }.disposed(by: self.disposeBag)
+        
+        self.todosRelay
+            .bind(to: self.tableView.rx.items(cellIdentifier: "TodoCell")) {
+                (index, todo: String, cell: UITableViewCell) in
+                cell.textLabel?.text = todo
+            }.disposed(by: self.disposeBag)
+    }
 }
 
